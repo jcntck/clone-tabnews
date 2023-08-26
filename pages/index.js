3d016a9c3add1c569abae5766cfec05ce4cf8e47
João Cshunderlick Neto <46545010+jcntck@users.noreply.github.com>
2023-08-26 20:57:06 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Sempre em busca e sem medo do erro, pois é no erro que se alcança a maestria.</h1>
+  return (
+    <h1>
+      Sempre em busca e sem medo do erro, pois é no erro que se alcança a
+      maestria.
+    </h1>
+  );
 }
 
 export default Home;
